3fb0af8f61914ed16cda81822c50da8f737de46a
Aluisco Miguel Ricardo Mastrapa <aluisco2005@gmail.com>
2024-02-05 12:42:58 -0500

    Fix backend and some names

--- lib/src/utils/constants.dart
+++ lib/src/utils/constants.dart
@@ -1,2 +1,3 @@
 // const site = 'https://api-locations.smcsalud.cu';
-const site = 'http://192.168.1.102:8000';
+// const site = 'http://192.168.1.102:8000';
+const site = 'http://192.168.0.3:8000';
